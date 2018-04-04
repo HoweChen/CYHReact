51cdf1ffa85f59cfa7cad6ce0b54c7e666202119
SkyFather <cyh278889789@gmail.com>
2018-04-05 00:10:39 +0800

    update app.js with eslint

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,9 +24,14 @@ var Header = function (_React$Component) {
         "div",
         null,
         React.createElement(
-          "p",
+          "h1",
           null,
-          "This is from Header"
+          this.props.title
+        ),
+        React.createElement(
+          "h2",
+          null,
+          this.props.subTitle
         )
       );
     }
@@ -35,15 +40,161 @@ var Header = function (_React$Component) {
   return Header;
 }(React.Component);
 
+var ActionButton = function (_React$Component2) {
+  _inherits(ActionButton, _React$Component2);
+
+  function ActionButton() {
+    _classCallCheck(this, ActionButton);
+
+    return _possibleConstructorReturn(this, (ActionButton.__proto__ || Object.getPrototypeOf(ActionButton)).apply(this, arguments));
+  }
+
+  _createClass(ActionButton, [{
+    key: "handlePick",
+    value: function handlePick() {
+      // let decision = Math.floor(Math.random() * this.props.todos.length);
+      // alert("You should do:" + (decision + 1) + ". " +
+      //   this.props.todos[decision]);
+      alert("fuck you");
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "button",
+          { onClick: this.handlePick },
+          "What should I do?"
+        )
+      );
+    }
+  }]);
+
+  return ActionButton;
+}(React.Component);
+
+var Options = function (_React$Component3) {
+  _inherits(Options, _React$Component3);
+
+  function Options() {
+    _classCallCheck(this, Options);
+
+    return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).apply(this, arguments));
+  }
+
+  _createClass(Options, [{
+    key: "render",
+    value: function render() {
+      var _this4 = this;
+
+      return React.createElement(
+        "ol",
+        null,
+        this.props.todos.map(function (todo) {
+          return React.createElement(
+            "li",
+            { key: _this4.props.todos.indexOf(todo) },
+            todo
+          );
+        })
+      );
+    }
+  }]);
+
+  return Options;
+}(React.Component);
+
+var AddTodo = function (_React$Component4) {
+  _inherits(AddTodo, _React$Component4);
+
+  function AddTodo() {
+    _classCallCheck(this, AddTodo);
+
+    return _possibleConstructorReturn(this, (AddTodo.__proto__ || Object.getPrototypeOf(AddTodo)).apply(this, arguments));
+  }
+
+  _createClass(AddTodo, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "form",
+          null,
+          "Please input some tasks."
+        )
+      );
+    }
+  }]);
+
+  return AddTodo;
+}(React.Component);
+
+var RemoveAllTodo = function (_React$Component5) {
+  _inherits(RemoveAllTodo, _React$Component5);
+
+  function RemoveAllTodo() {
+    _classCallCheck(this, RemoveAllTodo);
+
+    return _possibleConstructorReturn(this, (RemoveAllTodo.__proto__ || Object.getPrototypeOf(RemoveAllTodo)).apply(this, arguments));
+  }
+
+  _createClass(RemoveAllTodo, [{
+    key: "removeAllTodo",
+    value: function removeAllTodo() {
+      alert("fuck you again!");
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "button",
+        { onClick: this.removeAllTodo },
+        "Remove all todos."
+      );
+    }
+  }]);
+
+  return RemoveAllTodo;
+}(React.Component);
+
+var TodoApp = function (_React$Component6) {
+  _inherits(TodoApp, _React$Component6);
+
+  function TodoApp() {
+    _classCallCheck(this, TodoApp);
+
+    return _possibleConstructorReturn(this, (TodoApp.__proto__ || Object.getPrototypeOf(TodoApp)).apply(this, arguments));
+  }
+
+  _createClass(TodoApp, [{
+    key: "render",
+    value: function render() {
+      var title = "This is a todo app";
+      var subTitle = "Hello there!";
+      var todos = ["test1", "test2", "test3"];
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Header, { title: title, subTitle: subTitle }),
+        React.createElement(ActionButton, { jobs: todos }),
+        React.createElement(AddTodo, null),
+        React.createElement(RemoveAllTodo, null),
+        React.createElement(Options, { todos: todos })
+      );
+    }
+  }]);
+
+  return TodoApp;
+}(React.Component);
+
 var jsx = React.createElement(
   "div",
   null,
-  React.createElement(
-    "h1",
-    null,
-    "Header"
-  ),
-  React.createElement(Header, null)
+  React.createElement(TodoApp, null)
 );
 
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById("todoApp"));
--- src/app.js
+++ src/app.js
@@ -1,8 +1,79 @@
 class Header extends React.Component {
-  render() {
+  render () {
     return (
       <div>
-        <p>This is from Header</p>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subTitle}</h2>
+      </div>
+    );
+  }
+}
+
+class ActionButton extends React.Component {
+  handlePick () {
+    // let decision = Math.floor(Math.random() * this.props.todos.length);
+    // alert("You should do:" + (decision + 1) + ". " +
+    //   this.props.todos[decision]);
+    alert("fuck you");
+  }
+
+  render () {
+    return (
+      <div>
+        <button onClick={this.handlePick}>What should I do?</button>
+      </div>
+
+    );
+  }
+}
+
+class Options extends React.Component {
+  render () {
+
+    return (
+      <ol>
+        {this.props.todos.map(todo => {
+          return <li key={this.props.todos.indexOf(todo)}>{todo}</li>;
+        })}
+      </ol>
+    );
+  }
+}
+
+class AddTodo extends React.Component {
+  render () {
+    return (
+      <div>
+        <form>Please input some tasks.</form>
+      </div>
+    );
+  }
+}
+
+class RemoveAllTodo extends React.Component {
+  removeAllTodo () {
+    alert("fuck you again!");
+  }
+
+  render () {
+    return (
+      <button onClick={this.removeAllTodo}>Remove all todos.</button>
+    );
+  }
+}
+
+class TodoApp extends React.Component {
+  render () {
+    const title = "This is a todo app";
+    const subTitle = "Hello there!";
+    const todos = ["test1", "test2", "test3"];
+    return (
+      <div>
+        <Header title={title} subTitle={subTitle}/>
+        <ActionButton jobs={todos}/>
+        <AddTodo/>
+        <RemoveAllTodo/>
+        <Options todos={todos}/>
       </div>
     );
   }
@@ -10,9 +81,8 @@ class Header extends React.Component {
 
 const jsx = (
   <div>
-    <h1>Header</h1>
-    <Header />
+    <TodoApp/>
   </div>
 );
 
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById("todoApp"));
