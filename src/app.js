032e9c9b32dbf0e9ce9d49989f1d7bfcd8d0e35f
SkyFather <cyh278889789@gmail.com>
2018-04-20 00:25:35 +0800

    update with utils.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import { square, addition } from "./utils.js";
+
 console.log("====================================");
 console.log("App js is running");
 console.log("====================================");
+console.log(square(4));
+console.log(addition(1, 2));
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+console.log("====================================");
+console.log("Util js is running.");
+console.log("====================================");
+
+const square = x => x * x;
+
+const addition = (a, b) => a + b;
+
+export { square, addition };
