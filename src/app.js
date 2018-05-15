d5664b4b0cc62eea57cca9197ece9b55f9ff5610
SkyFather <cyh278889789@gmail.com>
2018-05-15 15:23:38 +0800

    update normalize-css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TodoApp from "./components/TodoApp";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const jsx = (
