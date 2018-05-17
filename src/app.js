eb78e4245419ee285323fa88edddf5ab9710c00f
SkyFather <cyh278889789@gmail.com>
2018-05-17 16:51:04 +0800

    prepare for the new app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TodoApp from "./components/TodoApp";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const jsx = (
-  <div>
-    {/* <TodoApp todos={["Test1", "Test2", "Test3"]} /> */}
-    <TodoApp />
-  </div>
-);
+const jsx = <div />;
 
 ReactDOM.render(jsx, document.getElementById("todoApp"));
