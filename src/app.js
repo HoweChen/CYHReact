2ecf0962a3f8f51147fdb974fa4fb5402e23ab8e
SkyFather <cyh278889789@gmail.com>
2018-05-17 18:30:11 +0800

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const jsx = <div />;
+const ExpenseDashboardPage = () => {
+  return (
+    <div>
+      <p>This is from my dashboard component</p>
+    </div>
+  );
+};
 
-ReactDOM.render(jsx, document.getElementById("todoApp"));
+const AddExpensePage = () => {
+  return (
+    <div>
+      <p>This is from my add expense component</p>
+    </div>
+  );
+};
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("todoApp"));
