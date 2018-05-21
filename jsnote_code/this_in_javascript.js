a1d38e901b5cde07aaaefc98d1eae3dcc9c525dc
SkyFather <cyh278889789@gmail.com>
2018-05-22 01:11:43 +0800

    update

--- jsnote_code/this_in_javascript.js
+++ jsnote_code/this_in_javascript.js
@@ -11,7 +11,23 @@ var object = {
   }
 };
 
+var test3 = {
+  a: "10",
+  somefunc: function() {
+    a = 11;
+    console.log(this.a);
+    console.log(a);
+    let another = () => {
+      let a = 12;
+      console.log(a);
+    };
+    another();
+  }
+};
+
 var test = object.getNameFunc();
 
 console.log(test());
 console.log(this.name);
+
+var sth3 = test3.somefunc();
